a06dcce725cd0d5e2f08b5d1e5247cffba0aca30
Abhinav Pottabathula <apottabathula@berkeley.edu>
2021-04-06 13:58:16 -0700

    Sending emotional states

--- app.py
+++ app.py
@@ -13,24 +13,18 @@ from deepface import DeepFace
 app = Flask(__name__)
 CORS(app)
 
+startTime = time.time()
+
 @app.route('/getScore', methods=['POST'])
 def getScore():
     data = request.data # width = 320, height = 240, rgba values, reference: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Uint8ClampedArray
     data = np.array(json.loads(data))
     data = data.reshape((240, 320, 4)).astype('uint8')
 
-    # print(data)
-    # print("\n\n\n\n")
-    # plt.imshow(data, interpolation='nearest')
-    # plt.show()
-    
-    img = Image.fromarray(data, 'RGBA')
-    # img.save('face.png')
-    # img.show()
-    pred = DeepFace.analyze(img, actions=['emotion'])
-    print(pred)
-    score = np.random.rand()
-    return json.dumps({"score":score, "timestamp": time.time()})
+    pred = DeepFace.analyze(data, actions=['emotion'])
+    pred['timestamp'] = time.time() - startTime
+
+    return pred
 
 def getEmotions(img):
     return
